9232dc3bf5e32797bb492da4a66d897ad41664b5
DavidMaxwell616 <33132201+DavidMaxwell616@users.noreply.github.com>
2020-11-18 22:29:38 -0800

    update

--- Maxteroids/js/boot.js
+++ Maxteroids/js/boot.js
@@ -1,19 +1,22 @@
-var MAXENEMIES = 100;
-var MAXROCKS = 100;
-var BIGROCK = 50;
-var SMALLROCK = 20;
-var NumRocks = 5;
-var NumParticles = 5;
-var ShipExplodeCount = 100;
-var NumEnemies = 5;
-var ShipSize = 30;
-//var shipPic;
+// var MAXENEMIES = 100;
+// var MAXROCKS = 100;
+// var BIGROCK = 50;
+// var SMALLROCK = 20;
+// var NumRocks = 5;
+// var NumParticles = 5;
+// var ShipExplodeCount = 100;
+// var NumEnemies = 5;
+// var ShipSize = 30;
 var gameOverText;
 var gameOver = false;
-var Score=0;
-var HighScore=0;
-var RocksLeft;
-var ShipsLeft;
+let bullets = [];
+let asteroids = [];
+let score = 0;
+let lives = 3;
+let highScore;
+let localStorageName = "Maxteroids";
+// var RocksLeft;
+// var ShipsLeft;
 var gameStart = false;
 var introTextSize = .01;
 var introText;
@@ -23,21 +26,43 @@ var leftKey;
 var rightKey;
 var upKey;
 var downKey;
-var MainShip;
-var Enemies = [];
-var Explosion = [];
-var Particles = [];
-var Rocks = [];
-var Bullets = [];
-var EnemyBullets = [];
-var Pie = 3.14159265358979;
-var Radians = (2 * Pie) / 360;
-var shipLine = [];
-var bulletLine = [];
- var enemyBulletLine = [];
-var enemyLine = [];
-var ShipsLeft = 5;
-var RockNumStart = 4;
+var player;
+// var Enemies = [];
+// var Explosion = [];
+// var Particles = [];
+// var Rocks = [];
+// var Bullets = [];
+// var EnemyBullets = [];
+// var shipLine = [];
+// var bulletLine = [];
+//  var enemyBulletLine = [];
+// var enemyLine = [];
+// var ShipsLeft = 5;
+// var RockNumStart = 4;
 var Level = 1;
 var infoText2;
 var bulletup = 1;
+var player = {};
+
+var playerShape = [
+  { x: 18,  y: 0},
+  { x: -18, y: -12},
+  { x: -10, y: -8},
+  { x: -10, y: 8},
+  { x: -18, y: 12},
+]
+const  introTextString = "Rotate Right - Right Arrow \n" +
+"Rotate Left - Left Arrow \n" +
+"Thrust - Up Arrow \n" +
+"Hyperspace - Down Arrow \n" +
+"Fire - Space Bar \n" +
+"Press Space to Start";
+
+
+
+   // Movement Directions
+   var DOWN = 0,
+   LEFT = -1,
+   RIGHT = 1;
+
+  var graphics;
